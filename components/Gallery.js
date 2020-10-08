1e06b3bf7ff2b3d444f28bd249b4229f8719a5d5
Nicolas Sayer <nicolascsayer@gmail.com>
2020-10-08 17:58:30 +0200

    Small fix

--- components/Gallery.js
+++ components/Gallery.js
@@ -2,17 +2,16 @@ import { chunk, shuffle } from "lodash/fp";
 
 import styles from "../styles/Gallery.module.css";
 
-const chunks = chunk(
-  6,
-  shuffle([...Array(73)].map((_, index) => index + 1))
-).filter((_, index) => index < 10);
-
 export default function Gallery() {
-  console.log(chunks);
+  const chunks = chunk(
+    6,
+    shuffle([...Array(73)].map((_, index) => index + 1))
+  ).filter((_, index) => index < 10);
+
   return (
     <div className={styles.grid}>
-      {chunks.map((array) => (
-        <div className={styles['vertical-grid']}>
+      {chunks.map((array, index) => (
+        <div key={index} className={styles["vertical-grid"]}>
           {array.map((id) => (
             <img
               key={id}
