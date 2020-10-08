bcfa399ecfb31859f4a9c7f15699c1cb01a9b9c4
Nicolas Sayer <nicolascsayer@gmail.com>
2020-10-08 19:07:26 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,17 @@ import { chunk, shuffle } from "lodash/fp";
 import Head from "next/head";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
 const NUMBER_OF_PICTURES = 73;
 
 export default function Home() {
+  const [test, setTest] = useState(0);
   const chunks = chunk(
     6,
     shuffle([...Array(NUMBER_OF_PICTURES)].map((_, index) => index + 1))
   ).filter((_, index) => index < 10);
-  console.log(chunks)
+  console.log(chunks);
 
   return (
     <div className={styles.layout}>
@@ -20,6 +22,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <div
+        onClick={() => {
+          setTest(test + 1);
+        }}
+      >
+        {test}
+      </div>
       <div className={styles["grid-container"]}>
         <div className={styles.grid}>
           {chunks.map((array) => (
