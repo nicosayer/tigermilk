2096a0c0da3ec855a59383529d0913d18e6146a2
Nicolas Sayer <nicolascsayer@gmail.com>
2020-10-08 19:02:38 +0200

    chunks

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,12 @@ import styles from "../styles/Home.module.css";
 
 const NUMBER_OF_PICTURES = 73;
 
-export default function Home({ chunks }) {
+export default function Home() {
+  const chunks = chunk(
+    6,
+    shuffle([...Array(NUMBER_OF_PICTURES)].map((_, index) => index + 1))
+  ).filter((_, index) => index < 10);
+
   return (
     <div className={styles.layout}>
       <Head>
@@ -17,7 +22,7 @@ export default function Home({ chunks }) {
       <div className={styles["grid-container"]}>
         <div className={styles.grid}>
           {chunks.map((array) => (
-            <div key={array.join('')} className={styles["vertical-grid"]}>
+            <div key={array.join("")} className={styles["vertical-grid"]}>
               {array.map((id) => (
                 <img
                   key={id}
@@ -31,13 +36,4 @@ export default function Home({ chunks }) {
       </div>
     </div>
   );
-}
-
-export function getServerSideProps() {
-  const chunks = chunk(
-    6,
-    shuffle([...Array(NUMBER_OF_PICTURES)].map((_, index) => index + 1))
-  ).filter((_, index) => index < 10);
-
-  return { props: { chunks } };
 }
