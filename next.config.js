f4cdfb5ddddbe919c0ec0751090727f1eaa482d0
Nicolas Sayer <nicolascsayer@gmail.com>
2020-10-17 00:46:21 +0200

    exportPathMap

--- next.config.js
+++ next.config.js
@@ -4,10 +4,22 @@ module.exports = {
     { dev, dir, outDir, distDir, buildId }
   ) {
     return {
-      "/locations/brussels": { page: "/locations/[location].js", },
-      "/locations/paris-canal-st-martin": { page: "/locations/[location].js", },
-      "/locations/paris-sentier": { page: "/locations/[location].js", },
-      "/locations/paris-south-pigalle": { page: "/locations/[location].js", },
+      "/locations/brussels": {
+        page: "/locations/[location]",
+        query: { location: "brussels" },
+      },
+      "/locations/paris-canal-st-martin": {
+        page: "/locations/[location]",
+        query: { location: "paris-canal-st-martin" },
+      },
+      "/locations/paris-sentier": {
+        page: "/locations/[location]",
+        query: { location: "paris-sentier" },
+      },
+      "/locations/paris-south-pigalle": {
+        page: "/locations/[location]",
+        query: { location: "paris-south-pigalle" },
+      },
     };
   },
 };
