68403c1b5103c711386eb476daabccfb04e3dce6
Nicolas Sayer <nicolascsayer@gmail.com>
2020-10-23 14:39:37 +0200

    sort json

--- scripts/sortJson.js
+++ scripts/sortJson.js
@@ -4,10 +4,12 @@ const glob = require("glob");
 
 const root = path.join(__dirname, "..");
 
-const filePaths = glob.sync("/**/!(package-lock).json", { root });
+const filePaths = glob.sync("!(node_modules)/**/!(package-lock).json", { root });
 
 filePaths.forEach((filePath) => {
   try {
     sortJson.overwrite(filePath);
-  } catch (error) {}
+  } catch (error) {
+    console.log(`Failed to sort file ${filePath}`)
+  }
 });
