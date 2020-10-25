248d09497b63328989cac9e07fadda98e91f4927
Nicolas Sayer <nicolascsayer@gmail.com>
2020-10-25 11:56:28 +0100

    toast opaicty

--- components/Toast.js
+++ components/Toast.js
@@ -11,22 +11,27 @@ export const Toast = ({ top, right, bottom, left, ...rest }) => {
   }, []);
 
   return (
-    <Box
+    <div
       style={{
         opacity: opacity,
         transition: "opacity 0.5s ease-in",
-        position: "fixed",
-        top: top ? "20px" : undefined,
-        right: right ? "20px" : undefined,
-        bottom: bottom ? "20px" : undefined,
-        left: left ? "20px" : undefined,
-        backgroundColor: "white",
-        borderRadius: "classic",
-        boxShadow: "classic",
-        padding: "10px",
-        zIndex: 1,
       }}
-      {...rest}
-    />
+    >
+      <Box
+        style={{
+          position: "fixed",
+          top: top ? "20px" : undefined,
+          right: right ? "20px" : undefined,
+          bottom: bottom ? "20px" : undefined,
+          left: left ? "20px" : undefined,
+          backgroundColor: "white",
+          borderRadius: "classic",
+          boxShadow: "classic",
+          padding: "10px",
+          zIndex: 1,
+        }}
+        {...rest}
+      />
+    </div>
   );
 };
