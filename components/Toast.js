1cd519e3cb3caea2d47655aaefe56a4bd7e48f8c
Nicolas Sayer <nicolascsayer@gmail.com>
2020-10-25 12:02:04 +0100

    Toast loading

--- components/Toast.js
+++ components/Toast.js
@@ -2,36 +2,33 @@ import { Box } from "components/Box";
 import { useEffect, useState } from "react";
 
 export const Toast = ({ top, right, bottom, left, ...rest }) => {
-  const [opacity, setOpacity] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
-      setOpacity(1);
-    }, 500);
+      setLoading(false);
+    }, 200);
   }, []);
 
+  if (loading) {
+    return false;
+  }
+
   return (
-    <div
+    <Box
       style={{
-        opacity: opacity,
-        transition: "opacity 0.5s ease-in",
+        position: "fixed",
+        top: top ? "20px" : undefined,
+        right: right ? "20px" : undefined,
+        bottom: bottom ? "20px" : undefined,
+        left: left ? "20px" : undefined,
+        backgroundColor: "white",
+        borderRadius: "classic",
+        boxShadow: "classic",
+        padding: "10px",
+        zIndex: 1,
       }}
-    >
-      <Box
-        style={{
-          position: "fixed",
-          top: top ? "20px" : undefined,
-          right: right ? "20px" : undefined,
-          bottom: bottom ? "20px" : undefined,
-          left: left ? "20px" : undefined,
-          backgroundColor: "white",
-          borderRadius: "classic",
-          boxShadow: "classic",
-          padding: "10px",
-          zIndex: 1,
-        }}
-        {...rest}
-      />
-    </div>
+      {...rest}
+    />
   );
 };
